1802cd61bbdc8667d21ffa08da4f4cbece91b4a7
“Filipe.Santiago” <thekyd42@gmail.com>
2024-02-23 12:28:10 +0000

    text-update

--- src/Pages/AddHints.jsx
+++ src/Pages/AddHints.jsx
@@ -1,45 +1,65 @@
-import {useState} from 'react';
-import axios from 'axios';
-import {useNavigate} from 'react-router-dom';
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const API_URL = "https://json-server-backend-hyrule-how-to.onrender.com";
 
-function AddHints(){
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [image, setImage] = useState("");
+function AddHints() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
 
-   const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function handleSubmit(e){
-        e.preventDefault();
+  function handleSubmit(e) {
+    e.preventDefault();
 
-        const hint = {title, description, image};
+    const hint = { title, description, image };
 
-        axios.post(`${API_URL}/hints`, hint)
-        .then(()=> navigate("/hints"))
-        .catch((error)=> console.log(error));
-
-    }
-    return (
+    axios
+      .post(`${API_URL}/hints`, hint)
+      .then(() => navigate("/hints"))
+      .catch((error) => console.log(error));
+  }
+  return (
     <div className="form-container">
-      <form className='form'onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit}>
         <div className="form-group">
-        <label>Title</label>
-        <input value = {title} name="title" type="text" required onChange={(e)=> setTitle(e.target.value)}/>
+          <label>Title</label>
+          <input
+            value={title}
+            name="title"
+            type="text"
+            required
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="form-group-description">
-        <label>Description</label>
-        <input value = {description} name="description" type="text" required onChange={(e)=> setDescription(e.target.value)} />
+          <label>Description</label>
+          <textarea
+            value={description}
+            name="description"
+            type="text"
+            required
+            onChange={(e) => setDescription(e.target.value)}
+          />
         </div>
         <div className="form-group">
-        <label>Image</label>
-        <input value = {image} name="image" type="text" required onChange={(e)=> setImage(e.target.value)} />
+          <label>Image</label>
+          <input
+            value={image}
+            name="image"
+            type="text"
+            required
+            onChange={(e) => setImage(e.target.value)}
+          />
         </div>
-        <button className='form-submit-btn' type="submit">.°˖✧ Add Hint ✧˖°.</button>
+        <button className="form-submit-btn" type="submit">
+          .°˖✧ Add Hint ✧˖°.
+        </button>
       </form>
-      </div>
-    );
+    </div>
+  );
 }
 
 export default AddHints;
--- src/Pages/EditProjectPage.jsx
+++ src/Pages/EditProjectPage.jsx
@@ -1,75 +1,85 @@
-import {useEffect, useState} from 'react';
-import {useParams, useNavigate} from 'react-router-dom';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const API_URL = "https://json-server-backend-hyrule-how-to.onrender.com";
-function EditProjectPage(){
+function EditProjectPage() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [image, setImage] = useState("");
-  const {id} = useParams();
+  const { id } = useParams();
   const navigate = useNavigate();
 
-  useEffect(()=>{
-    axios.get(`${API_URL}/hints/${id}`).then((response)=>{
-      setTitle(response.data.title);
-      setDescription(response.data.description);
-      setImage(response.data.image);
-    }).catch((error)=> console.log(error))
-  }, [id])
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/hints/${id}`)
+      .then((response) => {
+        setTitle(response.data.title);
+        setDescription(response.data.description);
+        setImage(response.data.image);
+      })
+      .catch((error) => console.log(error));
+  }, [id]);
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const data = {title, description, image};
-    axios.put(`${API_URL}/hints/${id}`, data)
-    .then(()=>{
-      navigate("/hints");
-    })
-    .catch((error)=>{
-      console.log(error)
-    })
-  }
+    const data = { title, description, image };
+    axios
+      .put(`${API_URL}/hints/${id}`, data)
+      .then(() => {
+        navigate("/hints");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-  const deleteProject = () =>{
-    axios.delete(`${API_URL}/hints/${id}` ).then(()=>{
-      navigate("/hints");
-    })
-    .catch((error)=>console.log(error));
-  }
-  
+  const deleteProject = () => {
+    axios
+      .delete(`${API_URL}/hints/${id}`)
+      .then(() => {
+        navigate("/hints");
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="form-container">
-      <form className='form'onSubmit={handleSubmit}>
+      <form className="form" onSubmit={handleSubmit}>
         <div className="form-group">
-        <label>Title</label>
-        <input
-          type="text"
-          name="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+          <label>Title</label>
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="form-group-description">
-        <label>Description</label>
-        <input
-          type="text"
-          name="description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
+          <label>Description</label>
+          <textarea
+            type="text"
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
         </div>
         <div className="form-group">
-        <label>Image</label>
-        <input
-          type="text"
-          name="image"
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-        />
+          <label>Image</label>
+          <input
+            type="text"
+            name="image"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          />
         </div>
-        <button className='form-submit-btn' type = "submit">.°˖✧ Edit ✧˖°.</button>
+        <button className="form-submit-btn" type="submit">
+          .°˖✧ Edit ✧˖°.
+        </button>
       </form>
-      <button className='form-submit-btn'onClick={deleteProject}>✖ Delete ✖</button>
+      <button className="form-submit-btn" onClick={deleteProject}>
+        ✖ Delete ✖
+      </button>
     </div>
   );
 }
